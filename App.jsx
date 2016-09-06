62e4143b306f5c35b9eb567df20b375c3ab8e507
methodcase <methodcase@gmail.com>
2016-09-06 15:50:35 +0800

    changed folder

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Disp from './Disp.jsx'
+import Disp from './Components/Disp.jsx'
 
 class App extends React.Component{
   render(){
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # react-es6-webpack-simple
     npm install
     webpack-dev-server
+    http://localhost/webpack-dev-server/
